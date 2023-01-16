476967d8de770b8e463b1e9bd5eb16f8ac8a72bd
to_binio <tobias.frischmann1@gmail.com>
2023-01-16 14:08:08 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+WGPU SHAPES is a simple "renderer" which makes it easer to render basic shapes with wgpu.
+
+Best used with [wgpu_noboiler](https://crates.io/crates/wgpu-noboiler)
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! WGPU SHAPES is a simple "renderer" which makes it easer to render basic shapes with wgpu.
+//!
+//! best used with [wgpu_noboiler](https://crates.io/crates/wgpu-noboiler)
+
+
 pub mod shape_renderer;
 pub mod rect;
 pub mod oval;
